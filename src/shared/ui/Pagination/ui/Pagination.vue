c10e17a1e29c2d8c4ce04300e1aae5780e4bf657
NikitaTorbenko <2015nekitciti@gmail.com>
2025-05-27 15:08:20 +0300

    feat: create pagination

---
+++ src/shared/ui/Pagination/ui/Pagination.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  currentPage: number;
+  total: number;
+  perPage: number;
+}>();
+
+const emit = defineEmits(["change"]);
+
+const totalPages = computed(() => Math.ceil(props.total / props.perPage));
+</script>
+
+<template>
+  <div class="pagination">
+    <button
+      @click="emit('change', currentPage - 1)"
+      :disabled="currentPage === 1"
+    >
+      Назад
+    </button>
+    <span>Страница {{ currentPage }} из {{ totalPages }}</span>
+    <button
+      @click="emit('change', currentPage + 1)"
+      :disabled="currentPage === totalPages"
+    >
+      Вперед
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.pagination {
+  display: flex;
+  gap: 12px;
+  justify-content: center;
+  margin: 20px 0;
+}
+</style>
